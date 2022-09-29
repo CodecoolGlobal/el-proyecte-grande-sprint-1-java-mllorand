1532f81d1a35b11268097bde7d7b43332be42c1d
pitchforx <m.lorandlevente@gmail.com>
2022-09-29 14:42:49 +0200

    feat: Nest Navbar and Footer into App

--- frontend/el-proyecte-grande/src/App.js
+++ frontend/el-proyecte-grande/src/App.js
@@ -1,8 +1,12 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar></Navbar>
+      <div id="content"></div>
+      <Footer></Footer>
     </div>
   );
 }
