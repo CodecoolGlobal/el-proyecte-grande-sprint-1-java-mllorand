dba84166143292883e6d92e843dbeb09763c7939
csaszarattila42 <csaszarattila90@gmail.com>
2022-09-29 13:30:14 +0200

    feat: Add id field to AffinityLabel

--- backend/src/main/java/com/codecool/elproyectegrande/model/AffinityLabel.java
+++ backend/src/main/java/com/codecool/elproyectegrande/model/AffinityLabel.java
@@ -3,15 +3,21 @@ package com.codecool.elproyectegrande.model;
 //import java.awt.Color;
 
 public class AffinityLabel {
+    private final long id;
     private final String internalName;
     private final String displayName;
 
     //private final Color labelColor;
 
 
-    public AffinityLabel(String internalName, String displayName) {
+    public AffinityLabel(long id, String internalName, String displayName) {
+        this.id = id;
         this.internalName = internalName;
         this.displayName = displayName;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getInternalName() {
