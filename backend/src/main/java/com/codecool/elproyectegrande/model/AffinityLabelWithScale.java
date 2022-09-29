76fd7aba951f56ba2fb8faac85bb4c9136f45166
csaszarattila42 <csaszarattila90@gmail.com>
2022-09-29 10:53:26 +0200

    feat: Create class for storing users' chosen affinity labels with subjective scale

---
+++ backend/src/main/java/com/codecool/elproyectegrande/model/AffinityLabelWithScale.java
@@ -0,0 +1,17 @@
+package com.codecool.elproyectegrande.model;
+
+public class AffinityLabelWithScale extends AffinityLabelWithWeight<Integer> {
+    private static final int MINIMUM_SCALE = 1;
+    private static final int MAXIMUM_SCALE = 10;
+
+    public AffinityLabelWithScale(String internalName, String displayName, Integer weightQuantity) {
+        super(internalName, displayName, weightQuantity, "");
+        if (weightQuantity < MINIMUM_SCALE || weightQuantity > MAXIMUM_SCALE) {
+            throw new LabelScaleOutOfBounds(
+                String.format("Label scale cannot be lesser than %d and greater than %d",
+                    MINIMUM_SCALE,
+                    MAXIMUM_SCALE)
+            );
+        }
+    }
+}
