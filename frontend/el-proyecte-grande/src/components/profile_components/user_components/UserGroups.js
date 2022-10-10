0fc308b2bde374b6a8ae4c684d6158efb0f470a8
pitchforx <m.lorandlevente@gmail.com>
2022-10-10 13:43:48 +0200

    feat: Create UserGroups component

---
+++ frontend/el-proyecte-grande/src/components/profile_components/user_components/UserGroups.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UserGroups = () => {
+	return (
+		<section id="user-groups-container">
+			<p className='field-label'>My groups:</p>
+			<div className="group-img-container">
+				<img src="src/components/profile_components/user_components/UserGroups" alt="" className="group-img"/>
+				<img src="src/components/profile_components/user_components/UserGroups" alt="" className="group-img"/>
+				<img src="src/components/profile_components/user_components/UserGroups" alt="" className="group-img"/>
+				<img src="src/components/profile_components/user_components/UserGroups" alt="" className="group-img"/>
+				<img src="src/components/profile_components/user_components/UserGroups" alt="" className="group-img"/>
+			</div>
+		</section>
+	);
+};
+
+export default UserGroups;
