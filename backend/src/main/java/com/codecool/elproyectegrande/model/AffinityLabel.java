a85e02da03117e096bb78c3d2355eb6e8ea52d49
csaszarattila42 <csaszarattila90@gmail.com>
2022-10-10 10:28:30 +0200

    refactor: Make AffinityLabel a JPA managed entity

--- backend/src/main/java/com/codecool/elproyectegrande/model/AffinityLabel.java
+++ backend/src/main/java/com/codecool/elproyectegrande/model/AffinityLabel.java
@@ -2,29 +2,34 @@ package com.codecool.elproyectegrande.model;
 
 //import java.awt.Color;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Builder
 public class AffinityLabel {
-    private final long id;
-    private final String internalName;
-    private final String displayName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NonNull
+    private String internalName;
+    @NonNull
+    private String displayName;
 
     //private final Color labelColor;
-
 
     public AffinityLabel(long id, String internalName, String displayName) {
         this.id = id;
         this.internalName = internalName;
         this.displayName = displayName;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getInternalName() {
-        return internalName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
     }
 }
