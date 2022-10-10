49a94bc2cd27548dce8748d73daba74be021cf9c
pitchforx <m.lorandlevente@gmail.com>
2022-10-10 13:40:52 +0200

    chore: Update react-router-dom dep

--- frontend/el-proyecte-grande/src/App.js
+++ frontend/el-proyecte-grande/src/App.js
@@ -1,23 +1,18 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Profile from "./components/Profile";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Profile from "./components/profile_components/Profile";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Navbar/>
-        <div className="content">
-          <Switch>
-            <Route exact path='/profile'>
-              <Profile/>
-            </Route>
-          </Switch>
-        </div>
-        <Footer/>
-      </div>
-    </Router>
+    <>
+      <Router>
+          <Routes>
+            <Route path='/' element={<Layout children={<Home />}/>}/>
+            <Route path='/profile' element={<Layout children={<Profile id={}/>}/>}/>
+          </Routes>
+      </Router>
+    </>
   );
 }
 
