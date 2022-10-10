83ee8b5eefdb73b4fae15416d5b151b37b46e89c
csaszarattila42 <csaszarattila90@gmail.com>
2022-10-10 11:47:03 +0200

    refactor: Make CooperatorAttribute JPA managed

--- backend/src/main/java/com/codecool/elproyectegrande/model/CooperatorAttribute.java
+++ backend/src/main/java/com/codecool/elproyectegrande/model/CooperatorAttribute.java
@@ -1,19 +1,25 @@
 package com.codecool.elproyectegrande.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class CooperatorAttribute<T> {
-    private final T attributeValue;
-    private final Visibility visibility;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NonNull
+    private T attributeValue;
+    @Enumerated(EnumType.STRING)
+    private Visibility visibility;
 
     public CooperatorAttribute(T attributeValue, Visibility visibility) {
         this.attributeValue = attributeValue;
         this.visibility = visibility;
-    }
-
-    public T getAttributeValue() {
-        return attributeValue;
-    }
-
-    public Visibility getVisibility() {
-        return visibility;
     }
 }
