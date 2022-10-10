2d0b574a429f316b8d1efcc7d224cb2787153045
csaszarattila42 <csaszarattila90@gmail.com>
2022-10-10 21:03:16 +0200

    refactor: delete unused classes

--- backend/src/main/java/com/codecool/elproyectegrande/model/LabelScaleOutOfBounds.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.elproyectegrande.model;
-
-public class LabelScaleOutOfBounds extends IllegalArgumentException {
-    public LabelScaleOutOfBounds(String message) {
-        super(message);
-    }
-}
