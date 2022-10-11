9629ee1e91b2e1315dbdd921409cd800c2899dc0
csaszarattila42 <csaszarattila90@gmail.com>
2022-10-11 12:06:09 +0200

    fix: Introduce service layer to the stack

--- backend/src/main/java/com/codecool/elproyectegrande/controller/UserProfileController.java
+++ backend/src/main/java/com/codecool/elproyectegrande/controller/UserProfileController.java
@@ -2,6 +2,7 @@ package com.codecool.elproyectegrande.controller;
 
 import com.codecool.elproyectegrande.model.Cooperator;
 import com.codecool.elproyectegrande.persistance.CooperatorDAO;
+import com.codecool.elproyectegrande.service.CooperatorProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +11,22 @@ import org.springframework.web.bind.annotation.*;
 public class UserProfileController {
     private static final long DEFAULT_USER_ID = 1;
 
-    private final CooperatorDAO cooperatorDAO;
+    private final CooperatorProfileService cooperatorService;
 
-    public UserProfileController(CooperatorDAO cooperatorDAO) {
-        this.cooperatorDAO = cooperatorDAO;
+    public UserProfileController(CooperatorProfileService cooperatorService) {
+        this.cooperatorService = cooperatorService;
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<Cooperator> displayUserProfile(@PathVariable long userId) {
-        return cooperatorDAO.findById(userId)
+        return cooperatorService.findById(userId)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("")
     public ResponseEntity<Cooperator> displayDefaultUserProfile() {
-        return cooperatorDAO.findById(DEFAULT_USER_ID)
+        return cooperatorService.findById(DEFAULT_USER_ID)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
     }
---
+++ backend/src/main/java/com/codecool/elproyectegrande/service/CooperatorProfileService.java
@@ -0,0 +1,70 @@
+package com.codecool.elproyectegrande.service;
+
+import com.codecool.elproyectegrande.model.Cooperator;
+import com.codecool.elproyectegrande.model.StringAttribute;
+import com.codecool.elproyectegrande.persistance.*;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CooperatorProfileService {
+    private CooperatorDAO cooperatorDAO;
+    private AffinityLabelDAO affinityLabelDAO;
+    private AffinityLabelWithMonthsDAO affinityLabelWithMonthsDAO;
+    private InterestAffinityLabelDAO interestAffinityLabelDAO;
+    private GenderAttributeDAO genderAttributeDAO;
+    private IntegerAttributeDAO integerAttributeDAO;
+    private StringAttributeDAO stringAttributeDAO;
+
+    public <S extends Cooperator> S save(S entity) {
+        return cooperatorDAO.save(entity);
+    }
+
+    public <S extends Cooperator> Iterable<S> saveAll(Iterable<S> entities) {
+        return cooperatorDAO.saveAll(entities);
+    }
+
+    public Optional<Cooperator> findById(Long aLong) {
+        return cooperatorDAO.findById(aLong);
+    }
+
+    public boolean existsById(Long aLong) {
+        return cooperatorDAO.existsById(aLong);
+    }
+
+    public Iterable<Cooperator> findAll() {
+        return cooperatorDAO.findAll();
+    }
+
+    public long count() {
+        return cooperatorDAO.count();
+    }
+
+    public void deleteById(Long aLong) {
+        cooperatorDAO.deleteById(aLong);
+    }
+
+    public void delete(Cooperator entity) {
+        cooperatorDAO.delete(entity);
+    }
+
+    public void deleteAllById(Iterable<? extends Long> longs) {
+        cooperatorDAO.deleteAllById(longs);
+    }
+
+    public void deleteAll(Iterable<? extends Cooperator> entities) {
+        cooperatorDAO.deleteAll(entities);
+    }
+
+    public void deleteAll() {
+        cooperatorDAO.deleteAll();
+    }
+
+    public Optional<StringAttribute> getUserName(long userId) {
+        var cooperator = cooperatorDAO.findById(userId);
+        return cooperator.map(Cooperator::getUserName);
+    }
+}
