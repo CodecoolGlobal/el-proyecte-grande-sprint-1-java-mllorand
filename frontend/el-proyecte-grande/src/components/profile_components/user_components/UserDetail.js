8ed8207ea764d1b211bc339261d0f71b7c8c6bbe
pitchforx <m.lorandlevente@gmail.com>
2022-10-13 10:09:08 +0200

    feat: Implement two way binding for user fields and PUT update on button click

--- frontend/el-proyecte-grande/src/components/profile_components/user_components/UserDetail.js
+++ frontend/el-proyecte-grande/src/components/profile_components/user_components/UserDetail.js
@@ -1,13 +1,13 @@
 import React, {useContext, useEffect, useState} from 'react';
-import axios from "../../../apis/profileData";
+import axios from "axios";
 
 const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
 	const [field, setField] = useState(fieldValue);
 
-	const axiosPut = (fieldName, fieldValue) => {
-		axios.put(`http://localhost:8080/profile/2/${fieldName}`, {
+	const axiosPut = (fieldName, fieldValue, visibility) => {
+		axios.put(`/profile/2/${fieldName}`, {
 			"attributeValue": fieldValue,
-			"visibility": "PRIVATE"
+			"visibility": visibility
 		})
 	}
 
@@ -19,7 +19,7 @@ const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
 	}
 
 	const handleSaveBtn = (value) => {
-		save(fieldName, value)
+		axiosPut(fieldName, value, userData[fieldName].visibility)
 	}
 
 
@@ -28,7 +28,9 @@ const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
 			<span className='field-label'>{fieldName}:</span>
 			<input type='text' className='field' value={field}
 						 onChange={(e) => handleChange(e.target.value)}></input>
-			<button onClick={(e) => handleSaveBtn(e.target.value)}></button>
+			<button onClick={() => handleSaveBtn(field)}>
+				update
+			</button>
 		</div>
 	);
 }
