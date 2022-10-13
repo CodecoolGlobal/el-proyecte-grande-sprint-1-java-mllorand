0647f78dec97406f7e78459ddd3a6e3a453ddf0b
pitchforx <m.lorandlevente@gmail.com>
2022-10-13 13:27:49 +0200

    feat: Create select for visibility of user attributes

--- frontend/el-proyecte-grande/src/components/profile_components/user_components/UserDetail.js
+++ frontend/el-proyecte-grande/src/components/profile_components/user_components/UserDetail.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
 		const [field, setField] = useState(fieldValue);
+		const [visibility, setVisibility] = useState(userData[fieldName].visibility);
 
 		const axiosPatch = (fieldName, fieldValue, visibility) => {
 			axios.patch(`/profile/2/${fieldName}`, {
@@ -18,6 +19,13 @@ const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
 			setUserData(newUserData);
 		}
 
+		const handleChangeVisibility = (value) => {
+			let newUserData = {...userData}
+			newUserData[fieldName].visibility = value;
+			setVisibility(value);
+			setUserData(newUserData);
+		}
+
 		const handleSaveBtn = (value) => {
 			axiosPatch(fieldName, value, userData[fieldName].visibility)
 		}
@@ -29,20 +37,27 @@ const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
 				{
 					fieldName !== 'gender' &&
 					<input type='text' className='field' value={field}
-								 onChange={ e => handleChange(e.target.value) }
+								 onChange={e => handleChange(e.target.value)}
 					>
 					</input>
 				}
 				{
 					fieldName === 'gender' &&
 					<select className="field" value={field}
-									onChange={ e => handleChange(e.target.value) }
+									onChange={e => handleChange(e.target.value)}
 					>
 						<option value="MALE">Male</option>
 						<option value="FEMALE">Female</option>
 						<option value="OTHER">Other</option>
 					</select>
 				}
+				<select className="visibility-lvl" value={visibility}
+								onChange={e => handleChangeVisibility(e.target.value)}
+				>
+					<option value="PRIVATE">Private</option>
+					<option value="PUBLIC">Public</option>
+					<option value="GROUPS_ONLY">Groups only</option>
+				</select>
 				<button onClick={() => handleSaveBtn(field)}>
 					update
 				</button>
