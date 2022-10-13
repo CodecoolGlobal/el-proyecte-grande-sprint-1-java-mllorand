3448ea3424c80aa6bdb62ad7d4f18221ebeab991
pitchforx <m.lorandlevente@gmail.com>
2022-10-13 13:13:59 +0200

    feat: Create conditional templating for gender select

--- frontend/el-proyecte-grande/src/components/profile_components/user_components/UserDetail.js
+++ frontend/el-proyecte-grande/src/components/profile_components/user_components/UserDetail.js
@@ -2,38 +2,53 @@ import React, {useContext, useEffect, useState} from 'react';
 import axios from "axios";
 
 const UserDetail = ({id, fieldName, fieldValue, userData, setUserData}) => {
-	const [field, setField] = useState(fieldValue);
+		const [field, setField] = useState(fieldValue);
 
-	const axiosPatch = (fieldName, fieldValue, visibility) => {
-		axios.patch(`/profile/2/${fieldName}`, {
-			"attributeValue": fieldValue,
-			"visibility": visibility
-		})
+		const axiosPatch = (fieldName, fieldValue, visibility) => {
+			axios.patch(`/profile/2/${fieldName}`, {
+				"attributeValue": fieldValue,
+				"visibility": visibility
+			})
+		}
+
+		const handleChange = (value) => {
+			let newUserData = {...userData}
+			newUserData[fieldName].attributeValue = value;
+			setField(value);
+			setUserData(newUserData);
+		}
+
+		const handleSaveBtn = (value) => {
+			axiosPatch(fieldName, value, userData[fieldName].visibility)
+		}
+
+
+		return (
+			<div className="usr-detail">
+				<span className='field-label'>{fieldName}:</span>
+				{
+					fieldName !== 'gender' &&
+					<input type='text' className='field' value={field}
+								 onChange={ e => handleChange(e.target.value) }
+					>
+					</input>
+				}
+				{
+					fieldName === 'gender' &&
+					<select className="field" value={field}
+									onChange={ e => handleChange(e.target.value) }
+					>
+						<option value="MALE">Male</option>
+						<option value="FEMALE">Female</option>
+						<option value="OTHER">Other</option>
+					</select>
+				}
+				<button onClick={() => handleSaveBtn(field)}>
+					update
+				</button>
+			</div>
+		);
 	}
-
-	const handleChange = (value) => {
-		let newUserData = {...userData}
-		newUserData[fieldName].attributeValue = value;
-		setField(value);
-		setUserData(newUserData);
-	}
-
-	const handleSaveBtn = (value) => {
-		axiosPatch(fieldName, value, userData[fieldName].visibility)
-	}
-
-
-	return (
-		<div className="usr-detail">
-			<span className='field-label'>{fieldName}:</span>
-			<input type='text' className='field' value={field}
-						 onChange={(e) => handleChange(e.target.value)}></input>
-			<button onClick={() => handleSaveBtn(field)}>
-				update
-			</button>
-		</div>
-	);
-}
 ;
 
 export default UserDetail;
