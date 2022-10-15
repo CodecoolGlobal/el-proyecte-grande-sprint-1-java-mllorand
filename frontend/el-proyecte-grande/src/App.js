41a8501f09c76b333b54655128aae1e1fe66477c
pitchforx <m.lorandlevente@gmail.com>
2022-10-15 19:26:29 +0200

    chore: Wrap Profile with the ProfileContextProvider

--- frontend/el-proyecte-grande/src/App.js
+++ frontend/el-proyecte-grande/src/App.js
@@ -2,18 +2,23 @@ import Profile from "./components/profile_components/Profile";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import {ProfileContextProvider} from "./context/ProfileContext";
 
 function App() {
-  return (
-    <>
-      <Router>
-          <Routes>
-            <Route path='/' element={<Layout children={<Home />}/>}/>
-            <Route path='/profile' element={<Layout children={<Profile/>}/>}/>
-          </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Layout children={<Home/>}/>}/>
+					<Route path='/profile' element={<Layout children={
+						<ProfileContextProvider>
+							<Profile/>
+						</ProfileContextProvider>
+					}/>}/>
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
