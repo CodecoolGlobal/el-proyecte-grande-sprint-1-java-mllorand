ad6052f0136dffeadb63c83388de0c2516ee8779
pitchforx <m.lorandlevente@gmail.com>
2022-10-16 20:01:22 +0200

    feat: Improve modal to serve as adding interface to any of the current coop-detail types

--- frontend/el-proyecte-grande/src/components/profile_components/Modal.js
+++ frontend/el-proyecte-grande/src/components/profile_components/Modal.js
@@ -1,18 +1,41 @@
-import {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {ProfileContext} from "../../context/ProfileContext";
 
-const Modal = ({handleAdd}) => {
+const Modal = ({fieldName, handleAdd}) => {
 	const {labels} = useContext(ProfileContext);
+	const [prio, setPrio] = useState(null);
+	const [weightQuantity, setWeightQuantity] = useState(null);
+	const [weightUnit, setWeightUnit] = useState(null);
 
 	return (
 		<div className='coop-label-chooser-modal'>
 			{labels.map(label => (
 				<div className="modal-item detail-item"
 						 key={label.id}
-						 onClick={() => handleAdd(label)}
 				>
 					<img src={`/skill_icons/${label.internalName}.png`}
-							 alt={label.tooltipText}/>
+							 alt={label.tooltipText}
+							 onClick={() => {
+								 if (prio) handleAdd(label, prio);
+								 else if (weightQuantity && weightUnit) handleAdd(label, weightQuantity, weightUnit);
+								 else handleAdd(label);
+							 }}
+					/>
+					{fieldName === 'skills' && <input className='weight-quantity'
+																						onChange={(e) => setWeightQuantity(e.target.value)}
+					></input>}
+					{fieldName === 'skills' && <input className='weight-unit'
+																						onChange={(e) => setWeightUnit(e.target.value)}
+					></input>}
+					{fieldName === 'interests' &&
+						<select name="prio" id="prio"
+										onChange={(e) => setPrio(e.currentTarget.value)}
+						>
+							<option value="Primary">Primary</option>
+							<option value="Secondary">Secondary</option>
+							<option value="Tertiary">Tertiary</option>
+						</select>
+					}
 				</div>
 			))}
 		</div>
