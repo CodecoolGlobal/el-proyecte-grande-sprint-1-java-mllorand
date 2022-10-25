0c3c3448b5ef12c604ca171e65f36231f3137ebf
csaszarattila42 <csaszarattila90@gmail.com>
2022-10-25 09:33:11 +0200

    feat: Send all coopportunities to frontend

---
+++ backend/src/main/java/com/codecool/elproyectegrande/controller/CoOpportunityController.java
@@ -0,0 +1,24 @@
+package com.codecool.elproyectegrande.controller;
+
+import com.codecool.elproyectegrande.model.coopportunity.CoOpportunity;
+import com.codecool.elproyectegrande.service.CoOpportunityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin({"http://localhost:3000", "https://codecrush.postman.co"})
+@RequestMapping("/coopportunity")
+@RequiredArgsConstructor
+public class CoOpportunityController {
+    private final CoOpportunityService coOpportinityService;
+
+    @GetMapping("/coopportunities")
+    public Iterable<CoOpportunity> getAllCooperators() {
+        return coOpportinityService.findAllVisibleToCooperator(1);
+    }
+
+    @GetMapping("/coopportunities/{pageNumber}")
+    public Iterable<CoOpportunity> getAllCooperators(@PathVariable int pageNumber) {
+        return coOpportinityService.findAllVisibleToCooperator(pageNumber);
+    }
+}
