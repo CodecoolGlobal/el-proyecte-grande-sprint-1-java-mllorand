e069a61d530542d9f957506cd1298509fd182fd0
csaszarattila42 <csaszarattila90@gmail.com>
2022-10-26 13:54:00 +0200

    feat: Add new coopportunity

--- backend/src/main/java/com/codecool/elproyectegrande/controller/CoOpportunityController.java
+++ backend/src/main/java/com/codecool/elproyectegrande/controller/CoOpportunityController.java
@@ -3,7 +3,11 @@ package com.codecool.elproyectegrande.controller;
 import com.codecool.elproyectegrande.model.coopportunity.CoOpportunity;
 import com.codecool.elproyectegrande.service.CoOpportunityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @CrossOrigin({"http://localhost:3000", "https://codecrush.postman.co"})
@@ -20,5 +24,11 @@ public class CoOpportunityController {
     @GetMapping("/coopportunities/{pageNumber}")
     public Iterable<CoOpportunity> getAllCooperators(@PathVariable int pageNumber) {
         return coOpportinityService.findAllVisibleToCooperator(pageNumber - 1);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<CoOpportunity> createNewCoOpportunity(@RequestBody CoOpportunity coOpportunity) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/cooperator/save").toUriString());
+        return ResponseEntity.created(uri).body(coOpportinityService.saveCoOpportunity(coOpportunity));
     }
 }
