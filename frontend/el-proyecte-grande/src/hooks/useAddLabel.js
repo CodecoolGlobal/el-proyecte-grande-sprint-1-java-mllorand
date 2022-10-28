e069bbb857e0a630a6bd48b16f6567c55db498f2
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-10-28 12:26:04 +0200

    refactor: Simplify logic of useAddLable custom hook

--- frontend/el-proyecte-grande/src/hooks/useAddLabel.js
+++ frontend/el-proyecte-grande/src/hooks/useAddLabel.js
@@ -8,55 +8,19 @@ const useAddLabel = (cooperatorData, setCooperatorData) => {
 			}
 	};
 	return (
-		(label, fieldName, prio, tags, skillQuantity, skillUnit, requestType) => {
+		(updatedEntity, fieldName) => {
 			let newCooperatorData = {...cooperatorData}
-			if (prio && tags) {
-				const newInterest = {
-					label: label,
-					interestPriority: prio,
-					tags: tags
-				}
-				newCooperatorData[fieldName] = [...newCooperatorData[fieldName], newInterest]
-				const data = {
-					"label": label,
-					"interestPriority": prio,
-					"tags": tags
-				};
-				if (requestType === 'post') axios.post(`/api/cooperator/${fieldName}`, data, requestConfig)
-				else axios.patch(`/api/cooperator/${fieldName}`, data, requestConfig)
-				setCooperatorData(newCooperatorData);
-
-			} else if (skillQuantity && skillUnit) {
-				const newLearnt = {
-					id: null,
-					label: label,
-					skillQuantity: skillQuantity,
-					skillUnit: skillUnit
-				}
-				console.log(cooperatorData)
-				newCooperatorData[fieldName] = [...newCooperatorData[fieldName], newLearnt]
-				const data = {
-					'id': null,
-					"label": label,
-					"skillQuantity": skillQuantity,
-					"skillUnit": skillUnit
-				};
-				if (requestType === 'post') axios.post(`/api/cooperator/${fieldName}`, data, requestConfig)
-				else axios.patch(`/api/cooperator/${fieldName}`, data, requestConfig)
-				setCooperatorData(newCooperatorData);
-
+			if (updatedEntity.id) {
+				axios.post(`/api/cooperator/${fieldName}`, updatedEntity, requestConfig)
+				newCooperatorData[fieldName] = [...newCooperatorData[fieldName], updatedEntity]
 			} else {
-				console.log(newCooperatorData)
-				newCooperatorData[fieldName] = [...newCooperatorData[fieldName], label]
-				axios.post(`/api/cooperator/${fieldName}`, {
-					"id": label.id,
-					"internalName": label.internalName,
-					"toolTipText": label.toolTipText
-				}, requestConfig)
-				setCooperatorData(newCooperatorData);
+				axios.patch(`/api/cooperator/${fieldName}`, updatedEntity, requestConfig)
+				newCooperatorData[fieldName] = newCooperatorData[fieldName].map(skill => (
+					skill.id === updatedEntity.id ? updatedEntity : skill
+				))
 			}
-		}
-	)
+			setCooperatorData(newCooperatorData)
+		})
 }
 
 export default useAddLabel;
